4cf29b4e0e82e4ea07122d135d6088dd6a5ac4e1
omodudu <omodudu.ma@gmail.com>
2026-09-16 05:07:27 +0100

    updated home-manager nix settings

--- home.nix
+++ home.nix
@@ -1,171 +1,409 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 let
-# FIXED: Using the direct binary fetch method which is more reliable for non-flake setups
-  zen-browser = pkgs.callPackage (pkgs.fetchFromGitHub {
-    owner = "0xc000022070";
-    repo = "zen-browser-flake";
-    rev = "master";
-    sha256 = "sha256-BgkUmlOuFaaZQCnlhXkQ3/Fng65aq00tJQkpoeTn7Mw="; # Nix will fix this for us
-  }) { };
-in
 
+  # ------------------------------------------------------------
+  # Zen Browser
+  # ------------------------------------------------------------
+
+  zen-browser =
+    if (pkgs ? zen-browser) then
+      pkgs.zen-browser
+    else
+      pkgs.callPackage ({ fetchurl, appimageTools }:
+        appimageTools.wrapType2 {
+          pname = "zen-browser";
+          version = "latest";
+          src = fetchurl {
+            url = "https://github.com/zen-browser/desktop/releases/latest/download/zen-x86_64.AppImage";
+            hash = "sha256-NJcEhxUi4AhfO1BdYpAJSQ7vs/Bu5nqH6hBtyxOVzP4=";
+          };
+        }) { };
+
+
+  # ------------------------------------------------------------
+  # Fix dlib for Python 3.14 / Howdy
+  #
+  # Wipe out patches via custom patchPhase.
+  # ------------------------------------------------------------
+
+  python3Packages-fixed =
+    pkgs.python3Packages.overrideScope
+      (python-final: python-prev: {
+
+        dlib =
+          if (python-prev ? dlib) then
+            python-prev.dlib.overrideAttrs (_oldAttrs: {
+              patches = [ ];
+              cmakeFlags = [ ];
+              patchPhase = ''
+                runHook prePatch
+                echo "Skipping dlib patches..."
+                runHook postPatch
+              '';
+            })
+          else
+            null;
+
+
+        # ------------------------------------------------------------
+        # Fix face-recognition so it uses our fixed dlib
+        # ------------------------------------------------------------
+
+        face-recognition =
+          if (python-prev ? face-recognition) then
+            python-prev.face-recognition.overrideAttrs
+              (oldAttrs: {
+                propagatedBuildInputs =
+                  builtins.map
+                    (
+                      dep:
+                        if (builtins.baseNameOf (toString dep)) == "dlib"
+                        then python-final.dlib
+                        else dep
+                    )
+                    (oldAttrs.propagatedBuildInputs or [ ]);
+
+                buildInputs =
+                  builtins.map
+                    (
+                      dep:
+                        if (builtins.baseNameOf (toString dep)) == "dlib"
+                        then python-final.dlib
+                        else dep
+                    )
+                    (oldAttrs.buildInputs or [ ]);
+              })
+          else
+            null;
+
+
+        # ------------------------------------------------------------
+        # Make OpenCV available to Howdy as Python cv2
+        # ------------------------------------------------------------
+
+        opencv4 = python-prev.opencv4;
+      });
+
+
+  # ------------------------------------------------------------
+  # Howdy Python dependencies
+  #
+  # Howdy's pythonDeps directly contains:
+  #
+  #   dlib
+  #   elevate
+  #   face-recognition
+  #   keyboard
+  #   opencv4Full
+  #   pycairo
+  #   pygobject3
+  #
+  # Replace only dlib and face-recognition with our fixed versions.
+  # ------------------------------------------------------------
+
+  howdy-python-deps =
+    builtins.map
+      (
+        dep:
+          if dep == "dlib"
+          then
+            python3Packages-fixed.dlib
+          else if dep == "face-recognition"
+          then
+            python3Packages-fixed.face-recognition
+          else if dep == "opencv4Full"
+          then
+            python3Packages-fixed.opencv4
+          else
+            dep
+      )
+      pkgs.howdy.pythonDeps;
+
+
+  # ------------------------------------------------------------
+  # Python environment required by Howdy
+  # ------------------------------------------------------------
+
+  howdy-python-env =
+    python3Packages-fixed.python.withPackages
+      (_: howdy-python-deps);
+
+
+  # ------------------------------------------------------------
+  # Howdy using the fixed Python environment
+  # ------------------------------------------------------------
+
+  howdy-fixed =
+    if (pkgs ? howdy) then
+      pkgs.howdy.overrideAttrs
+        (oldAttrs: {
+          pythonDeps = howdy-python-deps;
+          pythonEnv = howdy-python-env;
+
+          mesonFlags =
+            builtins.map
+              (
+                flag:
+                  if builtins.match "^-Dpython_path=.*" flag != null
+                  then
+                    "-Dpython_path=${howdy-python-env}/bin/python"
+                  else
+                    flag
+              )
+              (oldAttrs.mesonFlags or [ ]);
+        })
+    else
+      null;
+
+
+  # ------------------------------------------------------------
+  # Caelestia shell with Howdy PAM integration
+  # ------------------------------------------------------------
+
+  caelestia-shell-fixed =
+    if (pkgs ? caelestia-shell) then
+      pkgs.caelestia-shell.overrideAttrs
+        (oldAttrs: {
+          src = /home/omodudu/.local/src/caelestia/shell;
+
+          patches = [ ];
+
+          postPatch = ''
+            true
+          '';
+
+          postInstall =
+            (oldAttrs.postInstall or "")
+            + (
+              if howdy-fixed != null then
+                ''
+                  install -Dm644 /dev/stdin \
+                    $out/share/caelestia-shell/assets/pam.d/howdy <<EOF
+                  #%PAM-1.0
+                  auth required ${howdy-fixed}/lib/security/pam_howdy.so max-tries=1
+                  EOF
+                ''
+              else
+                ""
+            );
+        })
+    else
+      null;
+
+in
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+  # ------------------------------------------------------------
+  # Home Manager identity
+  # ------------------------------------------------------------
+
   home.username = "omodudu";
   home.homeDirectory = "/home/omodudu";
-
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "25.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-
-  # Unnecessary error removal
+  home.stateVersion = "25.11";
   home.enableNixpkgsReleaseCheck = false;
 
-  # Portal Backend
-  xdg.portal = {
-  enable = true;
-
-  extraPortals = with pkgs; [
-    xdg-desktop-portal-hyprland
-    xdg-desktop-portal-gtk
+  home.sessionPath = [
+    "/home/omodudu/.local/bin"
   ];
 
-  config = {
-    common = {
-      default = [ "hyprland" "gtk" ];
+
+  # ------------------------------------------------------------
+  # Portal Backend
+  # ------------------------------------------------------------
+
+  xdg.portal = {
+    enable = true;
+
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-hyprland
+      xdg-desktop-portal-gtk
+    ];
+
+    config = {
+      common = {
+        default = [
+          "hyprland"
+          "gtk"
+        ];
+      };
     };
   };
-};
 
 
+  # ------------------------------------------------------------
+  # Packages
+  # ------------------------------------------------------------
+
+  home.packages =
+    builtins.filter (p: p != null) (
+      (with pkgs; [
+
+        # ---------------------------------------
+        # Caelestia
+        # ---------------------------------------
+
+        caelestia-shell-fixed
+        (if pkgs ? caelestia-cli then caelestia-cli else null)
 
 
-  # packages
-  home.packages = (with pkgs; [
-    wlroots
-    mesa
-    libdrm
-    hyprland
-    hyprlock
-    hypridle
-    hyprpaper
-    hyprpicker
-    hyprshot
-    hyprsunset
-    pipewire
-    wireplumber
-    rofi
-    overskride
-    bluez
-    bluez-tools
-    networkmanagerapplet
-    networkmanager_dmenu
-    yazi
-    swayosd
-    celluloid
-    mpv
-    matugen
-    wlogout
-    waybar
-    hyprland-qtutils
-    qt6.qtwayland
-    xdg-desktop-portal-gtk
-    xdg-desktop-portal-hyprland
-    xdg-desktop-portal
-    dbus
-    awww
+        # ---------------------------------------
+        # Caelestia / Hyprland dependencies
+        # ---------------------------------------
 
-    hyprpolkitagent
-    swaynotificationcenter
-    wl-clipboard
-    cliphist
-    wl-clip-persist
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-  ]) ++ [
-    # External community package
-    zen-browser.default
-  ];
+        uwsm
+        dart-sass
+        swappy
+        slurp
+        grim
+        brightnessctl
+        gpu-screen-recorder
+        glib
 
 
+        # ---------------------------------------
+        # Eww / Elkowar's Wacky Widget
+        # ---------------------------------------
+
+        eww
+
+
+        # ---------------------------------------
+        # Hyprland / desktop
+        # ---------------------------------------
+
+        howdy-fixed
+        hyprland
+        hyprlock
+        hypridle
+        hyprpaper
+        hyprpicker
+        hyprshot
+        hyprsunset
+
+
+        # ---------------------------------------
+        # Applications / utilities
+        # ---------------------------------------
+
+        rofimoji
+        rofi
+        wtype
+        bluez
+        bluez-tools
+        networkmanagerapplet
+        networkmanager_dmenu
+        yazi
+        swayosd
+        celluloid
+        mpv
+
+
+        # ---------------------------------------
+        # Theming / appearance
+        # ---------------------------------------
+
+        matugen
+        wlogout
+        waybar
+
+
+        # ---------------------------------------
+        # Qt / Wayland
+        # ---------------------------------------
+
+        hyprland-qtutils
+        qt6.qtwayland
+
+
+        # ---------------------------------------
+        # Wallpaper
+        # ---------------------------------------
+
+        awww
+
+
+        # ---------------------------------------
+        # Authentication
+        # ---------------------------------------
+
+        hyprpolkitagent
+
+
+        # ---------------------------------------
+        # Notifications
+        # ---------------------------------------
+
+        swaynotificationcenter
+
+
+        # ---------------------------------------
+        # Clipboard
+        # ---------------------------------------
+
+        wl-clipboard
+        cliphist
+        wl-clip-persist
+
+      ])
+
+      ++ [
+
+        # ---------------------------------------
+        # External Zen Browser package
+        # ---------------------------------------
+
+        (if zen-browser ? default then zen-browser.default else zen-browser)
+
+      ]
+    );
+
+
+  # ------------------------------------------------------------
+  # SwayOSD
+  # ------------------------------------------------------------
 
   services.swayosd = {
     enable = false;
   };
 
 
-
+  # ------------------------------------------------------------
+  # Yazi
+  # ------------------------------------------------------------
 
   programs.yazi = {
     enable = true;
-    enableZshIntegration = true; # This helps with shell navigation
+    enableZshIntegration = true;
   };
 
 
+  # ------------------------------------------------------------
+  # Home Manager dotfiles
+  # ------------------------------------------------------------
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
+  home.file = { };
 
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-  };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/omodudu/etc/profile.d/hm-session-vars.sh
-  #
+  # ------------------------------------------------------------
+  # Environment variables
+  # ------------------------------------------------------------
 
   home.sessionVariables = {
-    # Forces Zen (and Firefox) to use native Wayland
+
+    # Native Wayland for Zen / Firefox
     MOZ_ENABLE_WAYLAND = "1";
     NIXOS_OZONE_WL = "1";
+
+    # Wayland / Hyprland desktop context
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    XDG_SESSION_DESKTOP = "Hyprland";
   };
 
-  # Let Home Manager install and manage itself.
+
+  # ------------------------------------------------------------
+  # Home Manager
+  # ------------------------------------------------------------
+
   programs.home-manager.enable = true;
 }
